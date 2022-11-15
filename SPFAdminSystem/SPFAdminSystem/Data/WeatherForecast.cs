cb8d3b67ad24c2a95acb672a85c62215ab9d09d8
LucasP <lucas-l-h-p@hotmail.dk>
2022-11-15 12:47:48 +0100

    Revert "[Merge] UploadFile is Merged into Database"
    This reverts commit a632802865de565428f01dbe8d7328b3b988833a.

--- SPFAdminSystem/SPFAdminSystem/Data/WeatherForecast.cs
+++ SPFAdminSystem/SPFAdminSystem/Data/WeatherForecast.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿namespace SPFAdminSystem.Data
 {
     public class WeatherForecast
@@ -11,18 +10,4 @@
 
         public string? Summary { get; set; }
     }
-=======
-namespace SPFAdminSystem.Data
-{
-    public class WeatherForecast
-    {
-        public DateTime Date { get; set; }
-
-        public int TemperatureC { get; set; }
-
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
-        public string? Summary { get; set; }
-    }
->>>>>>> origin/UX---UploadFilePage
 }
--- SPFAdminSystem/SPFAdminSystem/Data/WeatherForecastService.cs
+++ SPFAdminSystem/SPFAdminSystem/Data/WeatherForecastService.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿namespace SPFAdminSystem.Data
 {
     public class WeatherForecastService
@@ -18,25 +17,4 @@
             }).ToArray());
         }
     }
-=======
-namespace SPFAdminSystem.Data
-{
-    public class WeatherForecastService
-    {
-        private static readonly string[] Summaries = new[]
-        {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
-        public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
-        {
-            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = startDate.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            }).ToArray());
-        }
-    }
->>>>>>> origin/UX---UploadFilePage
 }
