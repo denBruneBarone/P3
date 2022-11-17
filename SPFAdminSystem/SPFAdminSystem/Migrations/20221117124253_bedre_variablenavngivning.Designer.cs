6305ac504ace51bf8f9486a01467ce791b88cc6c
denBruneBarone <runeberhardt@gmail.com>
2022-11-17 13:57:08 +0100

    [add] Product page

--- SPFAdminSystem/SPFAdminSystem/Migrations/20221117124253_bedre_variablenavngivning.Designer.cs
+++ SPFAdminSystem/SPFAdminSystem/Migrations/20221117124253_bedre_variablenavngivning.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace SPFAdminSystem.Migrations
 {
     [DbContext(typeof(SpilforsyningContext))]
-    partial class SpilforsyningContextModelSnapshot : ModelSnapshot
+    [Migration("20221117124253_bedre_variablenavngivning")]
+    partial class bedrevariablenavngivning
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.0");
