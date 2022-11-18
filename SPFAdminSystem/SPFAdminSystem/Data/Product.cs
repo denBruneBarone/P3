ba357a6f26df6661d48e5d47fa78b391edd47cf7
denBruneBarone <runeberhardt@gmail.com>
2022-11-18 14:13:13 +0100

    commit staged

---
+++ SPFAdminSystem/SPFAdminSystem/Data/Product.cs
@@ -0,0 +1,20 @@
+﻿namespace SPFAdminSystem.Data
+{
+    public class Product
+    {
+        public ulong productID { get; set; }
+        public ulong barcode { get; set; }
+        public string inHouseTitle { get; set; }
+        public string GWStitle { get; set; }
+        public int group { get; set; }
+        public int inStock { get; set; }
+        public int inOrder { get; set; }
+        public int available { get; set; }
+        public int ordered { get; set; }
+        public int target { get; set; }
+        public int packSize { get; set; }
+        public int minOrder { get; set; }
+       
+    }
+    
+}
---
+++ SPFAdminSystem/SPFAdminSystem/IRepository/IProductRepository.cs
@@ -0,0 +1,10 @@
+﻿using SPFAdminSystem.Data;
+
+namespace SPFAdminSystem.IRepository
+{
+    public interface IProductRepository
+    {
+        List<Product> HandleExcel(string fileName);
+
+    }
+}
