7fa7f9984bcff4c780570c80b3854c1685622eaf
pbaekgaard <ZW57DP@student.aau.dk>
2022-11-18 09:48:34 +0100

    [Merge] Authenitcation + database

---
+++ SPFAdminSystem/SPFAdminSystem/Authentication/UserAccountService.cs
@@ -0,0 +1,29 @@
+﻿namespace SPFAdminSystem.Authentication
+{
+    public class UserAccountService
+    {
+        public List<UserAccount> Users;
+
+        public UserAccountService()
+        {
+            Users = new List<UserAccount>
+            {
+                new UserAccount{ UserName = "admin", Password = "admin", Role = "Administrator" },
+                new UserAccount{ UserName = "user", Password = "user", Role = "User" }
+            };
+        }
+
+        public UserAccount? GetByUserName(string userName)
+        {
+            return Users.FirstOrDefault(x => x.UserName == userName);
+        }
+        public void Add(UserAccount user)
+        {
+            if(user.UserName == null || user.Password == null || user.Role == null)
+            {
+                throw new ArgumentNullException();
+            }
+            Users.Add(user);
+        }
+    }
+}
