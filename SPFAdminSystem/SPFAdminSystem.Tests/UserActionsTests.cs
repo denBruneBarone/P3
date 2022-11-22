04a3549065f5ad0a2e888ecba36a17c44e17cb66
Mcklmo <87199710+Mcklmo@users.noreply.github.com>
2022-11-22 12:16:27 +0100

    [fix] now its working

---
+++ SPFAdminSystem/SPFAdminSystem.Tests/UserActionsTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SPFAdminSystem.Pages.DatabasePages;
+
+namespace SPFAdminSystem.Tests
+{
+    public class UserActionsTests : TestContext
+    {
+
+    }
+}
