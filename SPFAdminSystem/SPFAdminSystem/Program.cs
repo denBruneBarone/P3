c546da40206adb0b9e1731f1cea37481f9a5a280
Mcklmo <87199710+Mcklmo@users.noreply.github.com>
2022-11-25 10:47:06 +0100

    [FIX] deleted using which threw an error

--- SPFAdminSystem/SPFAdminSystem/Program.cs
+++ SPFAdminSystem/SPFAdminSystem/Program.cs
@@ -1,4 +1,3 @@
-using BlazorTable;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
