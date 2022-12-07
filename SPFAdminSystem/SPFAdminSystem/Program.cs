fceec136405e1698f349f832686e82a4c6b12311
pbaekgaard <pbakga21@student.aau.dk>
2022-12-07 12:48:30 +0100

    bruh

--- SPFAdminSystem/SPFAdminSystem/Program.cs
+++ SPFAdminSystem/SPFAdminSystem/Program.cs
@@ -1,3 +1,4 @@
+using BlazorTable;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Server.ProtectedBrowserStorage;
@@ -28,6 +29,10 @@ builder.Services.AddServerSideBlazor();
 builder.Services.AddScoped<ProtectedSessionStorage>();
 builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddBlazorTable();
+builder.Services.AddBlazorDownloadFile();
+
+
 
 
 var folder = Environment.SpecialFolder.LocalApplicationData;
