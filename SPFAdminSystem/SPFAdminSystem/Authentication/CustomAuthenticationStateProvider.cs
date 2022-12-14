f86f360e4dca1530de89fdcd608f17336f654541
Mcklmo <87199710+Mcklmo@users.noreply.github.com>
2022-12-14 15:29:06 +0100

    [update] added comments to CustomAuthStateProvider

--- SPFAdminSystem/SPFAdminSystem/Authentication/CustomAuthenticationStateProvider.cs
+++ SPFAdminSystem/SPFAdminSystem/Authentication/CustomAuthenticationStateProvider.cs
@@ -4,9 +4,14 @@ using System.Security.Claims;
 
 namespace SPFAdminSystem.Authentication
 {
+    // This class provides custom authentication state information by
+    // extending the AuthenticationStateProvider class
     public class CustomAuthenticationStateProvider : AuthenticationStateProvider
     {
+        // ProtectedSessionStorage is used to manage user sessions
         private readonly ProtectedSessionStorage _sessionStorage;
+
+        // This ClaimsPrincipal represents an anonymous user
         private ClaimsPrincipal _anonymous = new ClaimsPrincipal(new ClaimsIdentity());
 
         public CustomAuthenticationStateProvider(ProtectedSessionStorage sessionStorage)
@@ -14,15 +19,21 @@ namespace SPFAdminSystem.Authentication
             _sessionStorage = sessionStorage;
         }
 
+        // returns the current authentication state
         public override async Task<AuthenticationState> GetAuthenticationStateAsync()
         {
             try
             {
-                //await Task.Delay(5000);
+                // Get the user session from the session storage
                 var userSessionStorageResult = await _sessionStorage.GetAsync<UserSession>("UserSession");
+
                 var userSession = userSessionStorageResult.Success ? userSessionStorageResult.Value : null;
+
+                // If there is no user session, return an AuthenticationState with the anonymous user
                 if (userSession == null)
                     return await Task.FromResult(new AuthenticationState(_anonymous));
+
+                // Otherwise, create a ClaimsPrincipal with the user's claims and return an AuthenticationState with it
                 var claimsPrincipal = new ClaimsPrincipal(new ClaimsIdentity(new List<Claim>
                 {
                     new Claim(ClaimTypes.Name, userSession.UserName),
@@ -31,12 +42,14 @@ namespace SPFAdminSystem.Authentication
                 }, "CustomAuth"));
                 return await Task.FromResult(new AuthenticationState(claimsPrincipal));
             }
-            catch
+            catch (InvalidOperationException)
             {
+                // raised everytime the ProtectedSessionStorage.GetAsync method is called before a UserSession is set in the sessionStorage
+                // handled by returning an anonymous user
                 return await Task.FromResult(new AuthenticationState(_anonymous));
             }
         }
-
+        // informs all currently rendered components that a user has changed his authstate
         public async Task UpdateAuthenticationState(UserSession userSession)
         {
             ClaimsPrincipal claimsPrincipal;
